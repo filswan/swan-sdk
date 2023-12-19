89450ceed14a51ce9e9a65dc90c022f97d0745f4
arash khosravi <arash.khosravi@nbai.io>
2023-12-19 11:07:01 -0500

    Modify setup by PyPI docs

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 """ SwanSDK setup code """
 
-from setuptools import setup
+from setuptools import setup, find_packages  # type: ignore
 from pathlib import Path
 
 this_directory = Path(__file__).parent
@@ -15,7 +15,8 @@ setup(
     author="FilSwan",  # Placeholder for author's name
     author_email="author@email.com",  # Placeholder for author's email
     install_requires=["web3==5.31.1", "requests==2.28.1", "requests_toolbelt==0.10.1"],
-    packages=["swan", "swan.common", "swan.api"],
+    packages=find_packages(where="src"),  # Automatically find packages in src
+    package_dir={"": "src"},  # Define the src directory as where to find packages
     license="MIT",
     include_package_data=True,
     description="A python software development kit for Swan services",
@@ -24,32 +25,10 @@ setup(
     url="https://github.com/username/package",  # Placeholder for URL
     python_requires=">=3.10",  # Specifies the required Python version
     classifiers=[
-        # Placeholder for classifiers
-        # EXAMPLE
-        # "Development Status :: 3 - Alpha",
-        # "Intended Audience :: Developers",
-        # "License :: OSI Approved :: MIT License",
-        # "Programming Language :: Python :: 3.10",
-    ],
-    entry_points={
-        "console_scripts": [
-            # Placeholder for entry points
-            # "our_command = our_package.module:main_function",
-        ],
-    },
-    extras_require={
-        # Placeholder for extras
-        # "extra_feature": ["extra_dependency"],
-    },
-    package_data={
-        "package": [
-            # Placeholder for package data
-            # "data/*.txt",
-        ],
-    },
-    scripts=[
-        # Placeholder for scripts
-        # "bin/script.py",
-    ],
+        "Development Status :: 1 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.10",
+    ]
 
 )
